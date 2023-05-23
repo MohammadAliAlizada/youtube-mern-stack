b26c7dedc3420c82aab104076204d7f368183d69
MohammadAliAlizada <muhdalializada@gmail.com>
2023-05-24 00:23:22 +0500

    first commit building structure

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import './_app.scss';
+import { react, useState } from 'react';
+import Header from './components/header/Header';
+import Sidebar from './components/sidebar/Sidebar';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './components/screen/HomeScreen';
+
+function App() {
+  const [sidebar, toggleSidebar] = useState(false)
+
+  const handleToggleSidebar = () => toggleSidebar(value => !value)
+  return (
+    <>
+      <Header handleToggleSidebar={handleToggleSidebar} />
+      <div className="app__container border border-info">
+        <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
+        <Container fluid className="app__main border border-warning">
+          <HomeScreen />
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default App;
